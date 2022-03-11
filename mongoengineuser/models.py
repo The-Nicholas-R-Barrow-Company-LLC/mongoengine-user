540714b3f395e97938565f73858f7bb361c0432b
barrownicholas <me@nicholasrbarrow.com>
2022-03-11 15:15:29 -0500

    0.0.3

--- mongoengineuser/models.py
+++ mongoengineuser/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from datetime import datetime
 
 class User(Document):
-    _id = StringField(required=True, max_length=50, default=str(uuid4()))
+    _id = StringField(primary_key=True, required=True, max_length=50, default=str(uuid4()))
     email = StringField(required=True, unique=True) # from mongoengine.errors import NotUniqueError
     first_name = StringField(max_length=50)
     last_name = StringField(max_length=50)
